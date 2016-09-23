24043d05bd4a2376bf4fe180967a7e5fc9d52ae5
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-09-24 09:40:24 +1200

    Fix build process

--- vault.portable/tools/chocolateyInstall.ps1
+++ vault.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'vault.portable'
-$url = 'https://releases.hashicorp.com/vault/0.6.0/vault_0.6.0_windows_386.zip'
+$url = 'https://releases.hashicorp.com/vault/0.6.1/vault_0.6.1_windows_386.zip'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url64 = 'https://releases.hashicorp.com/vault/0.6.0/vault_0.6.0_windows_amd64.zip'
- 
+$url64 = 'https://releases.hashicorp.com/vault/0.6.1/vault_0.6.1_windows_amd64.zip'
+
 Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64
